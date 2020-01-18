88d1791363b939855fc3de6299c3bf2a991f5acf
Sri Ray <sri.ray@ro.co>
2020-01-18 18:19:49 -0500

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 git config --global user.email "hi@sri.so"
 git config --global user.name "$GITHUB_ACTOR"
 
+rm deploy.sh
+rm -rf .git
+rm generate.sh
+rm README.md
+rm template.html
+
 git add .
 git commit -m "new links"
 # git fetch --all
