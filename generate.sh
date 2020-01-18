97fbb5a67df4111b56972433752a69e9ad4bf6cf
Sri Ray <sri.ray@ro.co>
2020-01-18 12:06:47 -0500

    links

---
+++ generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while IFS=, read -r short_url long_url
+do
+	if [ ! -d "/$short_url" ]
+	then
+		mkdir $short_url
+		sed "s!LONG_URL!$long_url!g" template.html > $short_url/index.html
+	else
+		printf "\n Error: $short_url -> $long_url exists."
+	fi
+done < links.csv
