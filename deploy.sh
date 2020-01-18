a3cc88f588db525dc6a102d75ab46c3587d664a4
Sri Ray <sri.ray@ro.co>
2020-01-18 18:22:00 -0500

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git config --global user.email "hi@sri.so"
 git config --global user.name "$GITHUB_ACTOR"
 
 rm deploy.sh
-rm .github
+rm -rf .github
 rm generate.sh
 rm README.md
 rm template.html
@@ -13,4 +13,4 @@ git add .
 git commit -m "new links"
 # git fetch --all
 # git rebase origin/links
-git push -u origin HEAD:links --force
+git push -u origin HEAD:gh-pages --force
