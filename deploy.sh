6d150c3726032090044ede8136f79fe176f62f42
Sri Ray <sri.ray@ro.co>
2020-01-18 18:02:23 -0500

    push to links

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@
 git config --global user.email "hi@sri.so"
 git config --global user.name "sricola"
 
-git fetch --all
-git checkout origin/links
 git add .
 git commit -m "new links"
-git push -u origin links
+git push -u origin master:links
