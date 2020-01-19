3aa275d5355726380f00266f4f4c6b8936794e77
Sri Ray <sri.ray@ro.co>
2020-01-19 12:32:06 -0500

    cleanup

--- generate.sh
+++ generate.sh
@@ -2,12 +2,12 @@
 
 while IFS=, read -r short_url long_url
 do
-	if [ ! -d "$short_url" ]
+	if [ ! -d "public/$short_url" ]
 	then
-		mkdir $short_url
-		sed "s!LONG_URL!$long_url!g" template.html > $short_url/index.html
-        printf "Made: $short_url -> $long_url"
+		mkdir public/$short_url
+		sed "s!LONG_URL!$long_url!g" template.html > public/$short_url/index.html
+        printf "New: $short_url -> $long_url\n"
 	else
-		printf "\n Passing: $short_url -> $long_url exists."
+		printf "Exists: $short_url -> $long_url\n"
 	fi
 done < links.csv
