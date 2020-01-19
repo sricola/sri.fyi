5f1c9164f0f15d45c8404c32617af7cb3f9ab663
Sri Ray <hi@sri.so>
2020-01-19 17:56:17 -0500

    cleanup tabs

--- generate.sh
+++ generate.sh
@@ -4,10 +4,10 @@ while IFS=, read -r short_url long_url
 do
 	if [ ! -d "docs/$short_url" ]
 	then
-		mkdir docs/$short_url
-		sed "s!LONG_URL!$long_url!g" template.html > docs/$short_url/index.html
+        mkdir docs/$short_url
+        sed "s!LONG_URL!$long_url!g" template.html > docs/$short_url/index.html
         printf "New: $short_url -> $long_url\n"
 	else
-		printf "Exists: $short_url -> $long_url\n"
+        printf "Exists: $short_url -> $long_url\n"
 	fi
 done < links.csv
