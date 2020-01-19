7477b9f1d98ffd70b4b04e6c841fb7eeec7af687
Sri Ray <sri.ray@ro.co>
2020-01-19 12:34:45 -0500

    move to docs

--- generate.sh
+++ generate.sh
@@ -2,10 +2,10 @@
 
 while IFS=, read -r short_url long_url
 do
-	if [ ! -d "public/$short_url" ]
+	if [ ! -d "docs/$short_url" ]
 	then
-		mkdir public/$short_url
-		sed "s!LONG_URL!$long_url!g" template.html > public/$short_url/index.html
+		mkdir docs/$short_url
+		sed "s!LONG_URL!$long_url!g" template.html > docs/$short_url/index.html
         printf "New: $short_url -> $long_url\n"
 	else
 		printf "Exists: $short_url -> $long_url\n"
